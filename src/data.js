1afa5aff74c90457bdb7d302493eed43f6881131
rahuldalal99 <rahuldalal99@gmail.com>
2021-11-20 18:03:28 -0800

    changed the date format

--- src/data.js
+++ src/data.js
@@ -1,87 +1,98 @@
-export const pantry = [{
-    text: 'Graduate Housing',
+export const pantry = [
+  {
+    text: "Graduate Housing",
     id: 1,
-    color: '##ADD8E6',
-  }, {
-    text: 'Student Center A',
+    color: "##ADD8E6",
+  },
+  {
+    text: "Student Center A",
     id: 2,
-    color: '#ff9747',
-  }];
+    color: "#ff9747",
+  },
+];
 
-  export const data = [
-    {
-      //text: 'Open',
-      pantryID: [2],
-      startDate: new Date('2021-11-01,10:00:00.000'),
-      endDate: new Date('2021-11-01,12:00:00.000'),
-      recurrenceRule: 'FREQ=WEEKLY;BYDAY=MO,WE;COUNT=20',
-
-    }, {
-      //text: 'Open',
-      pantryID: [2],
-      startDate: new Date('2021-11-01,15:00:00.000'),
-      endDate: new Date('2021-06-01,17:00:00.000'),
-      recurrenceRule: 'FREQ=WEEKLY;BYDAY=MO;COUNT=20',
-    }, {
-      //text: 'Open',
-      pantryID: [2],
-      startDate: new Date('2021-11-02,13:00:00.000'),
-      endDate: new Date('2021-11-02,17:00:00.000'),
-      recurrenceRule: 'FREQ=WEEKLY;BYDAY=TU,TH;COUNT=20',
-    }, {
-      //text: 'Open',
-      pantryID: [2],
-      startDate: new Date('2021-11-03,13:00:00.000'),
-      endDate: new Date('2021-11-03,15:00:00.000'),
-      recurrenceRule: 'FREQ=WEEKLY;BYDAY=WE;COUNT=20',
-    }, {
-      //text: 'Open',
-      pantryID: [2],
-      startDate: new Date('2021-11-03,16:00:00.000'),
-      endDate: new Date('2021-11-03,18:00:00.000'),
-      recurrenceRule: 'FREQ=WEEKLY;BYDAY=WE;COUNT=20',
-    }, {
-      //text: 'Open',
-      pantryID: [2],
-      startDate: new Date('2021-11-05, 9:00:00.000'),
-      endDate: new Date('2021-11-05, 12:00:00.000'),
-      recurrenceRule: 'FREQ=WEEKLY;BYDAY=FR;COUNT=20',
-    }, {
-      //text: 'Open',
-      pantryID: [1],
-      startDate: new Date('2021-11-01, 11:00:00.000'),
-      endDate: new Date('2021-11-01, 13:00:00.000'),
-      recurrenceRule: 'FREQ=WEEKLY;BYDAY=MO;COUNT=20',
-    }, {
-      //text: 'Open',
-      pantryID: [1],
-      startDate: new Date('2021-11-01, 14:00:00.000'),
-      endDate: new Date('2021-11-01, 16:00:00.000'),
-      recurrenceRule: 'FREQ=WEEKLY;BYDAY=MO;COUNT=20',
-    }, {
-      //text: 'Open',
-      pantryID: [1],
-      startDate: new Date('2021-11-02, 9:00:00.000'),
-      endDate: new Date('2021-11-02, 12:00:00.000'),
-      recurrenceRule: 'FREQ=WEEKLY;BYDAY=TU;COUNT=20',
-    }, {
-      pantryID: [1],
-      startDate: new Date('2021-11-02, 13:00:00.000'),
-      endDate: new Date('2021-11-02, 16:00:00.000'),
-      recurrenceRule: 'FREQ=WEEKLY;BYDAY=TU;COUNT=20',
-    }, {
-      //text: 'Open',
-      pantryID: [1],
-      startDate: new Date('2021-11-03, 14:00:00.000'),
-      endDate: new Date('2021-11-03, 17:00:00.000'),
-      recurrenceRule: 'FREQ=WEEKLY;BYDAY=WE,TH;COUNT=20',
-    }, {
-      //text: 'Open',
-      pantryID: [1],
-      startDate: new Date('2021-11-05, 9:00:00.000'),
-      endDate: new Date('2021-11-05, 13:00:00.000'),
-      recurrenceRule: 'FREQ=WEEKLY;BYDAY=FR;COUNT=20',
-    }
-
-  ];
-
+export const data = [
+  {
+    //text: 'Open',
+    pantryID: [2],
+    startDate: new Date("2021-11-01T10:00:00.000"),
+    endDate: new Date("2021-11-01T12:00:00.000"),
+    recurrenceRule: "FREQ=WEEKLY;BYDAY=MO,WE;COUNT=20",
+  },
+  {
+    //text: 'Open',
+    pantryID: [2],
+    startDate: new Date("2021-11-01T15:00:00.000"),
+    endDate: new Date("2021-06-01T17:00:00.000"),
+    recurrenceRule: "FREQ=WEEKLY;BYDAY=MO;COUNT=20",
+  },
+  {
+    //text: 'Open',
+    pantryID: [2],
+    startDate: new Date("2021-11-02T13:00:00.000"),
+    endDate: new Date("2021-11-02T17:00:00.000"),
+    recurrenceRule: "FREQ=WEEKLY;BYDAY=TU,TH;COUNT=20",
+  },
+  {
+    //text: 'Open',
+    pantryID: [2],
+    startDate: new Date("2021-11-03T13:00:00.000"),
+    endDate: new Date("2021-11-03T15:00:00.000"),
+    recurrenceRule: "FREQ=WEEKLY;BYDAY=WE;COUNT=20",
+  },
+  {
+    //text: 'Open',
+    pantryID: [2],
+    startDate: new Date("2021-11-03T16:00:00.000"),
+    endDate: new Date("2021-11-03T18:00:00.000"),
+    recurrenceRule: "FREQ=WEEKLY;BYDAY=WE;COUNT=20",
+  },
+  {
+    //text: 'Open',
+    pantryID: [2],
+    startDate: new Date("2021-11-05T09:00:00.000"),
+    endDate: new Date("2021-11-05T12:00:00.000"),
+    recurrenceRule: "FREQ=WEEKLY;BYDAY=FR;COUNT=20",
+  },
+  {
+    //text: 'Open',
+    pantryID: [1],
+    startDate: new Date("2021-11-01T11:00:00.000"),
+    endDate: new Date("2021-11-01T13:00:00.000"),
+    recurrenceRule: "FREQ=WEEKLY;BYDAY=MO;COUNT=20",
+  },
+  {
+    //text: 'Open',
+    pantryID: [1],
+    startDate: new Date("2021-11-01T14:00:00.000"),
+    endDate: new Date("2021-11-01T16:00:00.000"),
+    recurrenceRule: "FREQ=WEEKLY;BYDAY=MO;COUNT=20",
+  },
+  {
+    //text: 'Open',
+    pantryID: [1],
+    startDate: new Date("2021-11-02T09:00:00.000"),
+    endDate: new Date("2021-11-02T12:00:00.000"),
+    recurrenceRule: "FREQ=WEEKLY;BYDAY=TU;COUNT=20",
+  },
+  {
+    pantryID: [1],
+    startDate: new Date("2021-11-02T13:00:00.000"),
+    endDate: new Date("2021-11-02T16:00:00.000"),
+    recurrenceRule: "FREQ=WEEKLY;BYDAY=TU;COUNT=20",
+  },
+  {
+    //text: 'Open',
+    pantryID: [1],
+    startDate: new Date("2021-11-03T14:00:00.000"),
+    endDate: new Date("2021-11-03T17:00:00.000"),
+    recurrenceRule: "FREQ=WEEKLY;BYDAY=WE,TH;COUNT=20",
+  },
+  {
+    //text: 'Open',
+    pantryID: [1],
+    startDate: new Date("2021-11-05T09:00:00.000"),
+    endDate: new Date("2021-11-05T13:00:00.000"),
+    recurrenceRule: "FREQ=WEEKLY;BYDAY=FR;COUNT=20",
+  },
+];
