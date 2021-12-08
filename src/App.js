2b98edc82012ce374c7be9a122ad638e9d276fdb
rahuldalal99 <rahuldalal99@gmail.com>
2021-12-08 13:34:18 -0800

    fixed refreshing bug

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
-import 'devextreme/dist/css/dx.common.css';
-import 'devextreme/dist/css/dx.light.css';
+import "devextreme/dist/css/dx.common.css";
+import "devextreme/dist/css/dx.light.css";
 import "./App.css";
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MyScheduler from './Component/Scheduler'
-import Navbar from './Component/Navbar';
-import MyGallery from './Component/Gallery';
-import Categories from './Component/categories';
-import React from 'react';
-import Chatbot from './Component/Chatbot';
-import ContactUs from './Component/ContactUs';
-import AboutUs from './Component/AboutUs';
-import { ImageList } from '@material-ui/core';
-import { Url } from 'devextreme-react/chart';
-import background from "./Images/bg_logo.png"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MyScheduler from "./Component/Scheduler";
+import Navbar from "./Component/Navbar";
+import MyGallery from "./Component/Gallery";
+import Categories from "./Component/categories";
+import React from "react";
+import Chatbot from "./Component/Chatbot";
+import ContactUs from "./Component/ContactUs";
+import AboutUs from "./Component/AboutUs";
+import { ImageList } from "@material-ui/core";
+import { Url } from "devextreme-react/chart";
+import background from "./Images/bg_logo.png";
 
 function App() {
-        return(
-            <div className="App" style={{backgroundSize:"cover",backgroundImage: `url(${background})` ,backgroundRepeat: 'repeat-y',backgroundSize:"100%", backgroundAttachment:"fixed"}}>
-                
-            <Router>
-            <Navbar />
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "100%",
+        backgroundAttachment: "fixed",
+      }}
+    >
+      <Router>
+        <Navbar />
 
-                    <Routes>
-                    <Route path='/' exact element={<AboutUs/>} />
-                    <Route path='/gradhousing' element={<Categories/>} />
-                    <Route path='/studentcenter' element={<Categories/>} />
-                    <Route path='/contactus' element={<ContactUs/>} />
-                    <Route path='/mygallery' element={<MyGallery/>} />
-                    <Route path='/chatbot' element={<Chatbot/>}  />
-                    <Route path='/scheduler' element={<MyScheduler/>} >
-                    </Route>
-                    </Routes>
-            </Router>
-            </div>
-        )
-    }
+        <Routes>
+          <Route path="/" exact element={<AboutUs />} />
+          <Route path="/gradhousing" element={<Categories />} />
+          <Route path="/studentcenter" element={<Categories />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          {/* <Route path="/mygallery" element={<MyGallery />} /> */}
+          <Route path="/chatbot" element={<Chatbot />} />
+          <Route path="/scheduler" element={<MyScheduler />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 
 export default App;
--- src/Component/items.js
+++ src/Component/items.js
@@ -2,12 +2,16 @@ import react from "react";
 
 function Item({ name, category, quantity, imageurl }) {
   return (
-    <div className="item text-center">
+    <div className="item text-center justify">
       <img className="container" src={imageurl} alt="product.jpg" />
       <br />
-      <b style={{fontFamily:"Book Antiqua", fontSize: "1.4rem"}}>Product Name: {name}<br />
-      Quantity: {quantity}<br />
-      Category: {category}</b>
+      <b style={{ fontFamily: "Book Antiqua", fontSize: "1.4rem" }}>
+        Product Name: {name}
+        <br />
+        Quantity: {quantity}
+        <br />
+        Category: {category}
+      </b>
     </div>
   );
 }
